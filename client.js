903f5ece4e8247a7168655a4c1c25566c5565d7e
TimRChen <527410313@qq.com>
2017-03-16 22:49:13 +0800

    send message data

--- client.js
+++ client.js
@@ -3,25 +3,38 @@
  */
 
 let http = require('http');
+let qs = require('querystring');
 
 // 请求地址信息
 let info = {
     host: '127.0.0.1',
     port: 3000,
     url: '/',
-    method: 'GET'
+    method: 'POST'
 };
+
+let send = theName => {
+    http.request(info, res => {
+        res.setEncoding('utf8');
+        res.on('end', () => {
+            console.log('\n  \033[90m  request complete!\033[39m');
+            process.stdout.write('\n  your name: ');
+        });
+    }).end(qs.stringify({name: theName}));
+    // qs.stringify 将 name: theName 转化为 name=theName即查询字符串
+};
+
+// 用户输入提示
+process.stdout.write('\n  your name: ');
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', name => {
+    send(name.replace('\n', ''));
+});
+
+
 
 // 调用 request 方法，用于初始化一个新的http.Client Request对象
 // 调用完 request 方法后，还需要调用 end，原因是创建完一个请求后
 // 在发送给服务器前还可以和 request对象进行交互
-http.request(info, res => {
-    let body = '';
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-        body += chunk;
-    });
-    res.on('end', () => {
-        console.log('\n We got: \033[96m' + body + '\033[39m\n');
-    });
-}).end();
--- server.js
+++ server.js
@@ -4,9 +4,18 @@
  */
 
 let http = require('http');
+let qs = require('querystring');
 
 http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('Hello World');
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk;
+    });
+    req.on('end', () => {
+        res.writeHead(200);
+        res.end('Done');
+        console.log('\n  got name \033[90m' + qs.parse(body).name + '\033[39m\n');
+        // qs.parse 将name=theName 转化为 { name: theName }
+    });
 }).listen(3000);
 
